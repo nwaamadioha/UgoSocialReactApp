61fad0c480fedb962bcd47f6a59237b2426a1084
nwaamadioha <udokwu.temple960@gmail.com>
2022-12-31 10:58:43 +0000

    Third commit

--- SocialMediaApp/src/components/leftbar/LeftBar.jsx
+++ SocialMediaApp/src/components/leftbar/LeftBar.jsx
@@ -1,9 +1,25 @@
 import "./leftBar.scss"
+import Friends from "../../assets/1.png"
+
+
 
 
 const LeftBar = () => {
   return (
-    <div>Left Bar</div>
+    <div className="leftbar">
+      <div className="container">
+        <div className="menu">
+          <div className="user">
+            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <span>John Doe</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Friends</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
